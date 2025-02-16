a25ad759ee681f6701066de8b7d1fed43c2cffd2
UsuarioGithub <correo@github.com>
2025-02-16 16:33:57 -0500

    :)

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
 import Parser as P
 
 def main():
-    nombre_archivo = input("Dijite el archivo txt: ")  # Asegúrate de que el archivo exista en el mismo directorio que tu script o ajusta la ruta adecuadamente.
-    
-    # Procesamiento del archivo usando las funciones definidas
+    nombre_archivo = input("Dijite el archivo txt: ")  
     valid_tokens = P.tokenizer(nombre_archivo)
-    
-    # Intentar parsear los tokens válidos y manejar cualquier error de sintaxis
     try:
         P.parser(valid_tokens)
         print("Parsing completado con éxito.")
-        print(valid_tokens)  # Opcional: imprimir los tokens válidos para verificación
+        print(valid_tokens)
     except SyntaxError as e:
         print(str(e))
 
