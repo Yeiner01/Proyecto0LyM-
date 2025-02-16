eba370992f6d907aa97288a3605dbd1150644a83
UsuarioGithub <correo@github.com>
2025-02-16 17:25:55 -0500

    cambio P

--- mainP.py
+++ mainP.py
@@ -1,9 +1,10 @@
 from ParseP import check_file
+import ParseP as PP
 
 def main():
     filename = input("Ingrese el nombre del archivo a cargar (ej: caso_prueba.txt): ").strip()
-    result = check_file(filename)
+    result = PP.check_file(filename)
     print(result)
 
-if __name__ == '_main_':
+if __name__ == '__main__':
     main()
