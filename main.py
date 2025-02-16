826675cee957db70553c0b14e6f69a454338ecb2
Yeiner01 <y.ibarguenm@uniandes.edu.co>
2025-02-16 16:27:50 -0500

    arreglo main

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import Parser as P
 
 def main():
-    nombre_archivo = input("Ingrese el nombre del archivo (sin '.txt'): ").strip()
-    contenido = P.leer_archivo(nombre_archivo)
-    print("\nContenido del archivo:\n")
-    print(contenido)
+    nombre_archivo = "ejemplo.txt"  # Asegúrate de que el archivo exista en el mismo directorio que tu script o ajusta la ruta adecuadamente.
+    
+    # Procesamiento del archivo usando las funciones definidas
+    valid_tokens = P.tokenizer(nombre_archivo)
+    
+    # Intentar parsear los tokens válidos y manejar cualquier error de sintaxis
+    try:
+        P.parser(valid_tokens)
+        print("Parsing completado con éxito.")
+        print(valid_tokens)  # Opcional: imprimir los tokens válidos para verificación
+    except SyntaxError as e:
+        print(str(e))
 
-if __name__ == "_main_":
-    main() 
+if __name__ == "__main__":
+    main()
+
