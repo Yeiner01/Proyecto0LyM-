de7e3b38a066416dce0792485795595a58671681
Yeiner01 <y.ibarguenm@uniandes.edu.co>
2025-02-16 16:20:11 -0500

    aaaaaaaaaa

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import Parser as P
 
 def main():
-    nombre_archivo = input("Ingrese el nombre del archivo (sin '.txt'): ")
+    nombre_archivo = input("Ingrese el nombre del archivo (sin '.txt'): ").strip()
     contenido = P.leer_archivo(nombre_archivo)
     print("\nContenido del archivo:\n")
     print(contenido)
